dd6697d48ec9268c9b295f8b44d3fa261910a02d
Gabriel Woodruff <gabriel.o.woodruff@zisostudios.com>
2025-02-07 19:55:17 -0500

    New class for modular odometry auto.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libs/AR/OdometryAutoPath.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.Libs.AR;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+public class OdometryAutoPath {
+    public void addTarget(Pose2D target) {
+
+    }
+
+    public void updateState() {
+
+    }
+}
